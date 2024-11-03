57e6533dc18de516a02d3149bf0ea48bec2b53ab
Mithun Verma <88966956+Mithun20522@users.noreply.github.com>
2024-11-03 18:42:25 +0530

    fronted deployed

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://vector-shift-assignment.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
